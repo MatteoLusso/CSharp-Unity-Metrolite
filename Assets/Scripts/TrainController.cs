e30133bb5e0a6e538d789bb0fad1ffddd9fa38d6
MatteoLusso <matteo.lusso@outlook.com>
2023-04-16 14:21:38 +0200

    ID-19 Navigazione sezioni con doppi binari - Commit 1
    - Gestione navigazione linea con binari doppi

--- Assets/Scripts/TrainController.cs
+++ Assets/Scripts/TrainController.cs
@@ -11,6 +11,13 @@ public class TrainController : MonoBehaviour
         Backward,
     }
 
+    private enum Side
+    {
+        Center,
+        Right,
+        Left,
+    }
+
     public float speed = 0.0f;
     public float maxSpeed = 250.0f;
     public float acceleration = 50.0f;
@@ -18,13 +25,16 @@ public class TrainController : MonoBehaviour
     public float curveDrag = 0.1f;
     public float drag = 5.0f;
 
+    public float deadZoneTriggerRight = 0.1f;
+    public float deadZoneTriggerLeft = 0.1f;
+
     private Dictionary<string, List<LineSection>> lines;
     private AudioSource noise;
     private AudioSource braking;
 
     private bool goFowardActive = false;
     private bool goBackwardActive = false;
-    private float brakingNoiseDecreasingSpeed = 5.0f;
+    private float brakingNoiseDecreasingSpeed = 2.0f;
     private float previousSpeed = 0.0f;
     private Direction mainDir = Direction.None;
 
@@ -33,6 +43,8 @@ public class TrainController : MonoBehaviour
     private int indexSection = 0;
 
     private float heightCorrection;
+
+    private Side railSide = Side.Left;
 
     // Start is called before the first frame update
     void Start()
@@ -57,7 +69,7 @@ public class TrainController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         previousSpeed = speed;
         HandleMovement();
@@ -89,28 +101,40 @@ public class TrainController : MonoBehaviour
             goBackwardActive = false;
         }
 
-        if( Input.GetKey( KeyCode.D ) ) {
+        if( Input.GetKey( KeyCode.D ) || Input.GetAxis( "RT" ) > ( deadZoneTriggerRight ) ) {
+
+            float rightTriggerPression = 1.0f;
+            if( Input.GetAxis( "RT" ) > ( deadZoneTriggerRight ) ) {
+                rightTriggerPression = Input.GetAxis( "RT" );
+            }
+
+            Debug.Log( "rightTriggerPression: " + rightTriggerPression );
 
             if( mainDir == Direction.Forward || mainDir == Direction.None ) {
-                speed += acceleration * Time.fixedDeltaTime;
+                speed += acceleration * rightTriggerPression * Time.deltaTime;
             }
             else if( mainDir == Direction.Backward ) {
                 // In questo caso la speed è negativa
-                speed += deceleration * Time.fixedDeltaTime;
+                speed += deceleration * Time.deltaTime;
             }
 
             if( speed > maxSpeed ) {
                 speed = maxSpeed;
             }
         }
-        if( Input.GetKey( KeyCode.A ) ) {
+        if( Input.GetKey( KeyCode.A ) || Input.GetAxis( "LT" ) > ( deadZoneTriggerLeft ) ) {
+
+            float leftTriggerPression = 1.0f;
+            if( Input.GetAxis( "LT" ) > ( deadZoneTriggerLeft ) ) {
+                leftTriggerPression = Input.GetAxis( "LT" );
+            }
 
             if( mainDir == Direction.Backward || mainDir == Direction.None ) {
-                speed -= acceleration * Time.fixedDeltaTime;
+                speed -= acceleration * leftTriggerPression * Time.deltaTime;
             }
             else if( mainDir == Direction.Forward ) {
                 // In questo caso la speed è positiva
-                speed -= deceleration * Time.fixedDeltaTime;
+                speed -= deceleration * Time.deltaTime;
             }
 
             if( speed < -maxSpeed ) {
@@ -118,7 +142,7 @@ public class TrainController : MonoBehaviour
             }
         }
 
-        if( Mathf.Abs( speed ) >= acceleration * Time.fixedDeltaTime ) {
+        if( Mathf.Abs( speed ) >= acceleration * Time.deltaTime ) {
             if( mainDir == Direction.Forward ) {
                 speed -= drag * Time.deltaTime;
             }
@@ -126,14 +150,14 @@ public class TrainController : MonoBehaviour
                 speed += drag * Time.deltaTime;
             }
         }
-        else if( Mathf.Abs( speed ) <= acceleration * Time.fixedDeltaTime ) {
+        else if( Mathf.Abs( speed ) <= acceleration * Time.deltaTime ) {
             speed = 0.0f;
         }
     }
 
     private void HandleBrakingNoise() {
 
-        if( ( Input.GetKey( KeyCode.D ) && mainDir == Direction.Backward ) || ( Input.GetKey( KeyCode.A ) && mainDir == Direction.Forward ) ) { 
+        if( ( ( Input.GetKey( KeyCode.D ) || Input.GetAxis( "RT" ) > ( deadZoneTriggerRight ) ) && mainDir == Direction.Backward ) || ( ( Input.GetKey( KeyCode.A ) || Input.GetAxis( "LT" ) > ( deadZoneTriggerLeft ) ) && mainDir == Direction.Forward ) ) { 
 
             Debug.Log( "Braking" );
             if( !braking.isPlaying ) {
@@ -144,16 +168,17 @@ public class TrainController : MonoBehaviour
             else {
                 braking.volume = Mathf.Lerp( 0.0f, 1.0f, ( Mathf.Abs( speed ) / maxSpeed ) );
             }
-
         }
         else{
-            braking.volume = Mathf.Lerp( braking.volume, 0.0f, brakingNoiseDecreasingSpeed * Time.fixedDeltaTime );
+            braking.volume = Mathf.Lerp( braking.volume, 0.0f, brakingNoiseDecreasingSpeed * Time.deltaTime );
 
             if( braking.isPlaying && braking.volume <= 0.0f ) {
                 Debug.Log( "Stop Braking" );
                 braking.Stop();
             }
         }
+
+
     }
 
     IEnumerator GoForward()
@@ -166,7 +191,23 @@ public class TrainController : MonoBehaviour
 
                 Debug.Log( "Section: " + i );
 
-                List<Vector3> points = sections[ i ].bezierCurveLimitedAngle;
+                List<Vector3> points = null;
+                if( sections[ i ].type == Type.Tunnel ) {
+                    if( sections[ i ].bidirectional ) {
+                        if( railSide == Side.Left ) {
+                            points = sections[ i ].floorPoints.leftLine;
+                        }
+                        else if( railSide == Side.Right ) {
+                            points = sections[ i ].floorPoints.rightLine;
+                        }
+                    }
+                    else {
+                        points = sections[ i ].floorPoints.centerLine;
+                    }
+                }
+                else {
+                    points = sections[ i ].bezierCurveLimitedAngle;
+                }
                 
                 float deltaDist = 0.0f;
                 float dist = 0.0f;
@@ -176,6 +217,7 @@ public class TrainController : MonoBehaviour
                     // Distanza che sarà percorsa in un frame
                     deltaDist = Time.deltaTime * Mathf.Abs( speed );
                     Vector3 nextPoint = Vector3.zero;
+                    Vector3 nextOrientationPoint = Vector3.zero;
 
                     // Aggiornamento del movimento attuale del vagone (in questo caso avanti)
                     if( mainDir == Direction.Backward ) {
@@ -187,28 +229,60 @@ public class TrainController : MonoBehaviour
                     }
                     mainDir = Direction.Forward;
 
+                    int startingIndex = indexPoint;
                     // Ricerca del punto della curva successivo più lontano della distanza (per frame) percorsa dal vagone, che diventerà
                     // il punto successivo verso cui si dirigerà il vagone
                     for( int j = indexPoint; j < points.Count; j++ ) {
+                        int indexDiff = j - startingIndex;
                         dist = ( points[ j ] - this.transform.position ).magnitude;
 
                         indexPoint = j;
                         nextPoint = points[ j ];
+
+                        if( j + indexDiff >= points.Count ) {
+                            if( i + 1 < sections.Count ) {
+                                indexDiff = j + indexDiff - ( points.Count - 1 );
+
+                                if( sections[ i + 1 ].type == Type.Tunnel ) {
+                                    if( sections[ i + 1 ].bidirectional ) {
+                                        if( railSide == Side.Left ) {
+                                            nextOrientationPoint = sections[ i + 1 ].floorPoints.leftLine[ indexDiff ];
+                                        }
+                                        else if( railSide == Side.Right ) {
+                                            nextOrientationPoint = sections[ i + 1 ].floorPoints.rightLine[ indexDiff ];
+                                        }
+                                    }
+                                    else {
+                                        nextOrientationPoint = sections[ i + 1 ].floorPoints.centerLine[ indexDiff ];
+                                    }
+                                }
+                                else {
+                                    nextOrientationPoint = sections[ i + 1 ].bezierCurveLimitedAngle[ indexDiff ];
+                                }
+                            }
+                            else {
+                                nextOrientationPoint = points[ points.Count - 1 ];
+                            }
+                        }
+                        else {
+                            nextOrientationPoint = points[ j + indexDiff ];
+                        }
+
+
                         if( dist > deltaDist ) {
-                            //Debug.DrawLine( this.transform.position, nextPoint, Color.cyan, 1.0f );
+                            //Debug.Log( "indexDiff: " + indexDiff );
                             break;
                         }
                     }
-
-                    Debug.Log( "indexPoint: " + indexPoint );
                     
                     // Posizione e rotazione iniziale del vagone per punto della curva
-                    Vector3 nextDir = nextPoint - this.transform.position;
+                    Vector3 nextDir = nextOrientationPoint - this.transform.position;
                     Vector3 startDir = this.transform.right;
                     Vector3 startPos = this.transform.position;
                     float sumDist = 0.0f;
-                    Debug.DrawLine( startPos, nextPoint, Color.green, 1.0f );
                     while( sumDist < ( nextPoint - startPos ).magnitude ) {
+                        Debug.DrawLine( this.transform.position, nextPoint, Color.red, Time.deltaTime );
+                        Debug.DrawLine( this.transform.position, nextOrientationPoint, Color.magenta, Time.deltaTime );
 
                         // Interpolazione lineare (sulla distanza) per gestire posizione e rotazione frame successivo
                         this.transform.position = Vector3.Lerp( startPos, nextPoint, sumDist / dist ); 
@@ -216,8 +290,7 @@ public class TrainController : MonoBehaviour
 
                         noise.pitch = Mathf.Abs( speed ) / maxSpeed;
 
-                        Debug.DrawRay( this.transform.position, this.transform.right * 20, Color.red, 1.0f );
-                        Debug.DrawLine( this.transform.position, nextPoint, Color.cyan, Time.deltaTime );
+                        //Debug.DrawRay( this.transform.position, this.transform.right * 20, Color.red, 1.0f );
 
                         sumDist += Time.deltaTime * Mathf.Abs( speed );
 
@@ -243,7 +316,23 @@ public class TrainController : MonoBehaviour
 
                 Debug.Log( "Section: " + i );
 
-                List<Vector3> points = sections[ i ].bezierCurveLimitedAngle;
+                List<Vector3> points = null;
+                if( sections[ i ].type == Type.Tunnel ) {
+                    if( sections[ i ].bidirectional ) {
+                        if( railSide == Side.Left ) {
+                            points = sections[ i ].floorPoints.leftLine;
+                        }
+                        else if( railSide == Side.Right ) {
+                            points = sections[ i ].floorPoints.rightLine;
+                        }
+                    }
+                    else {
+                        points = sections[ i ].floorPoints.centerLine;
+                    }
+                }
+                else {
+                    points = sections[ i ].bezierCurveLimitedAngle;
+                }
                 
                 float deltaDist = 0.0f;
                 float dist = 0.0f;
@@ -253,6 +342,7 @@ public class TrainController : MonoBehaviour
                     // Distanza che sarà percorsa in un frame
                     deltaDist = Time.deltaTime * Mathf.Abs( speed );
                     Vector3 previousPoint = Vector3.zero;
+                    Vector3 previousOrientationPoint = Vector3.zero;
 
                     // Aggiornamento del movimento attuale del vagone (in questo caso indietro)
                     if( mainDir == Direction.Forward ) {
@@ -264,41 +354,76 @@ public class TrainController : MonoBehaviour
                     }
                     mainDir = Direction.Backward;
 
+                    int startingIndex = indexPoint;
                     // Ricerca del punto della curva precedente più lontano della distanza (per frame) percorsa dal vagone, che diventerà
                     // il punto successivo verso cui si dirigerà il vagone
                     for( int j = indexPoint; j >= 0; j-- ) {
+                        int indexDiff = startingIndex - j;
                         dist = ( points[ j ] - this.transform.position ).magnitude;
 
                         indexPoint = j;
                         previousPoint = points[ j ];
+
+                        if( j - indexDiff < 0 ) {
+                            if( i - 1 >= 0 ) {
+                                indexDiff = ( sections[ i - 1 ].bezierCurveLimitedAngle.Count - 1 ) + ( j - indexDiff );
+                                
+                                if( sections[ i - 1 ].type == Type.Tunnel ) {
+                                    if( sections[ i - 1 ].bidirectional ) {
+                                        if( railSide == Side.Left ) {
+                                            previousOrientationPoint = sections[ i - 1 ].floorPoints.leftLine[ indexDiff ];
+                                        }
+                                        else if( railSide == Side.Right ) {
+                                            previousOrientationPoint = sections[ i - 1 ].floorPoints.rightLine[ indexDiff ];
+                                        }
+                                    }
+                                    else {
+                                        previousOrientationPoint = sections[ i - 1 ].floorPoints.centerLine[ indexDiff ];
+                                    }
+                                }
+                                else {
+                                    previousOrientationPoint = sections[ i - 1 ].bezierCurveLimitedAngle[ indexDiff ];
+                                }
+                            }
+                            else {
+                                previousOrientationPoint = points[ 0 ];
+                            }
+                        }
+                        else {
+                            previousOrientationPoint = points[ j - indexDiff ];
+                        }
+
                         if( dist > deltaDist ) {
+                            Debug.Log( "indexPoint: " + indexPoint );
                             break;
                         }
                     }
-
-                    Debug.Log( "indexPoint: " + indexPoint );
                     
                     // Posizione e rotazione iniziale del vagone per punto della curva
-                    Vector3 previousDir = this.transform.position - previousPoint;
+                    Vector3 previousDir = this.transform.position - previousOrientationPoint;
                     Vector3 startDir = this.transform.right;
                     Vector3 startPos = this.transform.position;
                     float sumDist = 0.0f;
                     while( sumDist < ( previousPoint - startPos ).magnitude ) {
 
+                        Debug.DrawLine( this.transform.position, previousPoint, Color.red, Time.deltaTime );
+                        Debug.DrawLine( this.transform.position, previousOrientationPoint, Color.magenta, Time.deltaTime );
+
                         // Interpolazione lineare (sulla distanza) per gestire posizione e rotazione frame successivo
                         this.transform.position = Vector3.Lerp( startPos, previousPoint, sumDist / dist ); 
                         this.transform.right = Vector3.Slerp( startDir, previousDir, sumDist / dist );
+                        //Debug.DrawRay( this.transform.position, -this.transform.right * 20, Color.red, 1.0f );
 
                         noise.pitch = Mathf.Abs( speed ) / maxSpeed;
 
                         sumDist += Time.deltaTime * Mathf.Abs( speed );
 
-                        yield return new WaitForFixedUpdate();
+                        yield return new WaitForEndOfFrame();
                     }
                 }
 
                 // Aggiornamento index una volta raggiunto il punto iniziale della curva
-                if( i >= 0 ) {
+                if( i > 0 ) {
                     indexSection--;
                     indexPoint = sections[ indexSection ].bezierCurveLimitedAngle.Count - 1;
                 }
